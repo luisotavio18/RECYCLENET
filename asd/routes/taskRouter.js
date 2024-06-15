a85c93ba9d151ae5945ba99c32c9ace9c3c17020
Luis Otávio <126469734+luisotavio18@users.noreply.github.com>
2024-06-15 08:02:26 -0300

    Delete asd directory

--- asd/routes/taskRouter.js
+++
@@ -1,8 +0,0 @@
-const { Router } = require('express');
-const router = Router();
-
-const { storeTask } = require("../controller/taskController");
-
-router.post('/store/post', storeTask);
-
-module.exports = router;
