89d4d1afefe7084b8fb957dd4b6546290e4ff84f
LUIS OTAVIO ZAMBRZYCKI <03979057003@senacrs.edu.br>
2024-07-08 09:34:47 -0300

    json adicionado

--- mapas/coleta_seletiva/script.js
+++ mapas/coleta_seletiva/script.js
@@ -4,25 +4,27 @@ button.addEventListener('click', function(){
     alert('COLETA SELETIVA: \nHorário: das 8h às 17h30')
 })
 
+
+
+
+
 var map = L.map('map').setView([-29.765013289584562, -51.13489581599416], 13);
 L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
     maxZoom: 19,
     attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
     
-}).addTo(map);
+}).addTo(map.removeControl(map.zoomControl));
 
 
-function addMarker(lat, lng, txt) {
-    var marker = L.marker([lat, lng]).addTo(map);
-    marker.bindPopup(txt).openPopup();
-}
+/* Função para adicionar marcadores do arquivo "markers.json" */
 
-/* PONTO DAS COORDENADAS DE CADA LUGAR --------------------------------------------*/
-
-addMarker(-29.773298513426983, -51.16367441799755, "- Vincentina - <br>FREQUENCIA: Quinta-feira <br> HORÁRIO: das 8h às 17h30")
-addMarker(-29.76666091013486, -51.1465783594642, "- Centro e Fião - <br>FREQUENCIA: Segundo à sabado <br> HORÁRIO: das 8h às 17h30 <br> ")
-addMarker(-29.757733066884942, -51.09839226209659, "- Feitoria Seller - <br>FREQUENCIA: Quinta-feira <br> HORÁRIO: das 8h às 17h30 <br><br> INFORMAÇÕES ADICIONAIS:<br> Feitoria Seller sentido -> Integração")
-addMarker(-29.77414719456732, -51.150947275623295, "- Centro e Fião - <br>FREQUENCIA: Segundo à sabado <br> HORÁRIO: das 8h às 17h30 <br> ")
-addMarker(-29.76638870966359, -51.145615017891515, "- Rua Independência - <br>FREQUENCIA: Sexta-feira <br> HORÁRIO: das 8h às 17h30")
-addMarker(-29.779957697665118, -51.1397671895036, "- Morro do Espelho - <br>FREQUENCIA: Segunda-feira<br> HORÁRIO: das 8h às 17h30 <br> ")
-addMarker(-29.767563199241575, -51.13544963998137, "- São José - <br>FREQUENCIA: Sexta-feira <br> HORÁRIO: das 8h às 17h30")
+fetch('markers.json')
+            .then(response => response.json())
+            .then(data => {
+                // Adiciona os marcadores no mapa
+                data.forEach(markerData => {
+                    var marker = L.marker([markerData.lat, markerData.lng]).addTo(map)
+                        .bindPopup(`<b>${markerData.title}</b><br>${markerData.description}`);
+                });
+            })
+            .catch(error => console.log('Erro ao carregar o arquivo JSON: ', error));
