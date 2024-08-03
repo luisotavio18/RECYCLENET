28ac9c8cac3b811677e23d881dbbae0f514575fe
luisotavio18 <luisotavio0553@gmail.com>
2024-08-03 09:22:19 -0300

    mais markers adicionados

--- mapas/coleta_seletiva/script.js
+++ mapas/coleta_seletiva/script.js
@@ -1,10 +1,13 @@
-
 let button = document.getElementById("botao_info")
+let button2 = document.getElementById("botao_documento")
 
 button.addEventListener('click', function () {
     alert('COLETA SELETIVA: \nHorário: das 8h às 17h30')
 })
 
+button2.addEventListener('click', function() {
+    alert('Link do documento completo: \n\nhttps://leismunicipais.com.br/a/rs/s/sao-leopoldo/decreto/2018/901/9004/decreto-n-9004-2018-dispoe-sobre-o-plano-operacional-de-coleta-de-lixo-domiciliar-e-seletiva-que-tratam-os-artigos-273-280-290-291-292-293-294-e-295-do-codigo-municipal-de-meio-ambiente-e-da-outras-providencias ')
+})
 
 var map = L.map('map').setView([-29.765013289584562, -51.13489581599416], 13);
 L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
